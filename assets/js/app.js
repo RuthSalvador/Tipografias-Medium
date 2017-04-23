ad71b1d05d26832dc1d1897c63a96e982f0b8886
RuthSalvador <ruth.abby.sz@gmail.com>
2017-04-23 17:50:28 -0500

    JS: header lado derecho

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,13 +15,34 @@ window.addEventListener("load", function(){
     addSpan("sprite foto");
     addSpan("icon-twitter");
     addSpan("icon-facebook");
+    addSpan("follow");
 
     function addSpan(clase) {
       var span = document.createElement("span");
       span.setAttribute("class", clase);
       divLeft.appendChild(span);
+      //span.appendChild(document.createTextNode("x"));
     }
   }; addLeft();
+
+  /** Agregando lado derecho**/
+  function addRight(){
+    var divRight = document.createElement("nav");
+    header.appendChild(divRight);
+
+    addSpan("edit");
+    addSpan("icon-lupa");
+    addSpan("numero");
+    addSpan("sprite perfil");
+    addSpan("sprite logo");
+
+    function addSpan(clase) {
+      var span = document.createElement("span");
+      span.setAttribute("class", clase);
+      divRight.appendChild(span);
+    }
+  }; addRight();
+
 
 
 })
